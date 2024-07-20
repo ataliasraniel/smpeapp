dab10bc6fff6763fe5ff4edd9d6c6966e041e80d
Nicolas Mikael <nicolasmikael2001@gmail.com>
2024-07-20 19:28:55 -0300

    a

--- lib/screens/admin/screens/home/admin_home.dart
+++ lib/screens/admin/screens/home/admin_home.dart
@@ -10,11 +10,46 @@ class AdminHome extends StatefulWidget {
 class _AdminHomeState extends State<AdminHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(
+            label: 'Inicio',
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Configuracoes',
+            icon: Icon(Icons.settings),
+          ),
+        ]),
         body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: <Widget>[
-              Text('Olá, Admin!'),
-            ])));
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Sensores',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                "cameras",
+                style: TextStyle(fontSize: 24),
+              ),
+              ListTile(
+                title: Text("camera1"),
+              ),
+              ListTile(
+                title: Text(
+                  "camera2",
+                ),
+                subtitle: Text("olaaaa"),
+                trailing: Text("status: ATIVO"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
